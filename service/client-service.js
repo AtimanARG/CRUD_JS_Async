ca44d70c99d4c89a86acb575077ac15a82eee750
Atilio <marianoatilio@gmail.com>
2023-09-08 12:57:58 -0300

    Modulo - 2

--- service/client-service.js
+++ service/client-service.js
@@ -1,40 +1,3 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");    
-    const contenido = `
-        <td class="td" data-td>
-        ${nombre}
-        </td>
-        <td>
-        ${email}
-        </td>
-        <td>
-            <ul class="table__button-control">
-            <li>
-                <a
-                href="../screens/editar_cliente.html"
-                class="simple-button simple-button--edit"
-                >Editar</a
-                >
-            </li>
-            <li>
-                <button
-                class="simple-button simple-button--delete"
-                type="button"
-                >
-                Eliminar
-                </button>
-            </li>
-            </ul>
-        </td>
-            `;
-    linea.innerHTML = contenido;
-    return linea;
-};
-
-const table = document.querySelector("[data-table]")
-
-// Abrir HTTP (Método, URL)
-
 /* 
 CRUD Métodos HTTP
 C = Create (POST)
@@ -43,33 +6,11 @@ U = Update (PUT/PATCH)
 D = Delete (DELETE)
 */
 
-const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-        http.open("GET", "http://localhost:3000/perfil")
-        
-        http.send()
-        
-        http.onload = () => {
-            const response = JSON.parse(http.response)
-            if(http.status >= 400){ // Si es mayor a 400, es porque existio un error.
-                reject(response)
-            }
-            else {
-                resolve(response)
-            }
-        }
+//Fetch API
+const listaClientes = () => fetch("http://localhost:3000/perfil").then( (respuesta) => respuesta.json() );
 
-    })
 
-    return promise
-}
-
-listaClientes().then((data) => {
-    data.forEach(perfil => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-}).catch((error) => alert("Ocurrió un error"))
-
+export const clientServices = {
+    listaClientes,
+};
 
